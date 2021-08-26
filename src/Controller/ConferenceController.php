862bd636bc9fcd24754b3d147fbec2a38dcd794a
ebacour <ebacour78@gmail.com>
2021-08-26 10:39:55 +0200

    easter egg sur controller conference

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,17 +9,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/hello/{name}", name="homepage")
      */
-    public function index(): Response
+    public function index(string $name = ''): Response
     {
         // return $this->render('conference/index.html.twig', [
         //     'controller_name' => 'ConferenceController',
         // ]);
+        if ($name) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
         return new Response(<<<EOF
         <html>
             <body>
-                <img src"/images/under-construction.gif"/>
+                $greet
+                <img src="/images/under-construction.gif"/>
             </body>
         <html>
         EOF
